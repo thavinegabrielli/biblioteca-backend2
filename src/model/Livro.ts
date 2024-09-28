7382bc164a840c284b674bac41265d1c2f574f07
José Mateus <mateusjoseph@gmail.com>
2024-09-27 22:28:42 -0300

    Implementação Classe Livro e método listarLivros()

--- src/model/Livro.ts
+++ src/model/Livro.ts
@@ -28,6 +28,225 @@ export class Livro {
         this.statusLivroEmprestado = _statusLivroEmprestimo;
     }
 
+    //métodos GETTERS and SETTERS
+    /**
+     * Retorna o id do livro
+     * @returns id: idLivro
+     */
+    public getIdLivro(): number{
+        return this.idLivro;
+    }
+
+    /**
+     * Atribui o parâmetro ao atributo idAluno
+     * 
+     * @param _idLivro : idLivro
+     */
+    public setIdLivro(_idLivro: number): void{
+        this.idLivro = _idLivro;
+    }
+
+     /**
+     * Retorna o titulo do livro
+     * @returns titulo: _titulo
+     */
+    public getTitulo(): string{
+        return this.titulo;
+    }
+
+    /**
+     * Atribui o parâmetro ao atributo titulo
+     * 
+     * @param _titulo : titulo
+     */
+    public setTitulo(_titulo: string): void{
+        this.titulo = _titulo;
+    }
     
+     /**
+     * Retorna o autor do livro
+     * @returns autor: _autor
+     */
+     public getAutor(): string{
+        return this.autor;
+    }
+
+    /**
+     * Atribui o parâmetro ao atributo autor
+     * 
+     * @param _autor : autor
+     */
+    public setAutor(_autor: string): void{
+        this.autor = _autor;
+    }
+
+     /**
+     * Retorna a editora do livro
+     * @returns editora: _editora
+     */
+     public getEditora(): string{
+        return this.editora;
+    }
+
+    /**
+     * Atribui o parâmetro ao atributo editora
+     * 
+     * @param _editora : editora
+     */
+    public setEditora(_editora: string): void{
+        this.editora = _editora;
+    }
+
+     /**
+     * Retorna o ano de publicação do livro
+     * @returns anoPublicacao: _anoPublicacao
+     */
+     public getAnoPublicacao(): string{
+        return this.anoPublicacao;
+    }
+
+    /**
+     * Atribui o parâmetro ao ano de publicação titulo
+     * 
+     * @param _anoPublicacao : anoPublicacao
+     */
+    public setAnoPublicacao(_anoPublicacao: string): void{
+        this.anoPublicacao = _anoPublicacao;
+    }   
+
+    /**
+     * Retorna o ISBN do livro
+     * @returns isbn: _isbn
+     */
+public getISBN(): string{
+        return this.isbn;
+    }
+
+    /**
+     * Atribui o parâmetro ao atributo ISBN
+     * 
+     * @param _isbn : isbn
+     */
+    public setISBN(_isbn: string): void{
+        this.isbn = _isbn;
+    }
+
+    /**
+    * Retorna a quantidade total de livro
+    * @returns quantidade total: quantTotal
+    */
+    public getQuantTotal(): number{
+        return this.quantTotal;
+    }
+
+    /**
+     * Atribui o parâmetro ao atributo quantidade total
+     * 
+     * @param _quantTotal : quantTotal
+     */
+    public setQuantTotal(_quantTotal: number): void{
+        this.quantTotal = _quantTotal;
+    }
+
+    /**
+    * Retorna a quantidade disponivel de livro
+    * @returns quantidade disponivel: quantDisponivel
+    */
+    public getQuantDisponivel(): number{
+        return this.quantDisponivel;
+    }
+
+    /**
+     * Atribui o parâmetro ao atributo quantidade disponível
+     * 
+     * @param _quantDisponivel : quantDisponivel
+     */
+    public setQuantDisponivel(_quantDisponivel: number): void{
+        this.quantDisponivel = _quantDisponivel;
+    }
+
+    /**
+    * Retorna a quantidade total de livro
+    * @returns quantidade total: quantTotal
+    */
+    public getValorAquisicao(): number{
+        return this.valorAquisicao;
+    }
+
+    /**
+     * Atribui o parâmetro ao atributo valor aquisição
+     * 
+     * @param _valorAquisicao : valorAquisicao
+     */
+    public setValorAquisicao(_valorAquisicao: number): void{
+        this.valorAquisicao = _valorAquisicao;
+    }
+
+    public getStatusLivroEmprestado(): string{
+        return this.statusLivroEmprestado;
+    } 
+
+    public setStatusLivroEmprestado(_statusLivroEmprestimo: string): void{
+        this.statusLivroEmprestado = _statusLivroEmprestimo;
+    }
+
+    // MÉTODO PARA ACESSAR O BANCO DE DADOS
+    // CRUD Create - READ - Update - Delete
+
+    /**
+     * Retorna uma lista com todos os livros cadastrados no banco de dados
+     * 
+     * @returns Lista com todos os livros cadastrados no banco de dados
+     */
+    static async listarLivros(): Promise<Array<Livro> | null> {
+        // Criando lista vazia para armazenar os livros
+        let listaDeLivros: Array<Livro> = [];
+
+        try {
+            // Query para consulta no banco de dados
+            const querySelectLivro = `SELECT * FROM Livro;`;
+
+            // executa a query no banco de dados
+            const respostaBD = await database.query(querySelectLivro);
+
+            // percorre cada resultado retornado pelo banco de dados
+            // livro é o apelido que demos para cada linha retornada do banco de dados
+            respostaBD.rows.forEach((livro) => {
+                // criando objeto livro
+                let novoLivro = new Livro(
+                    livro.titulo,
+                    livro.autor,
+                    livro.editora,
+                    livro.anoPublicacao,
+                    livro.isbn,
+                    livro.quantTotal,
+                    livro.quantDisponivel,
+                    livro.valorAquisicao,
+                    livro.statusLivroEmprestado
+                );
+                // adicionando o ID ao objeto
+                novoLivro.setIdLivro(livro.id);
+                
+                // adicionando um livro na lista
+                listaDeLivros.push(novoLivro);
+            });
+
+            // retornado a lista de livros para quem chamou a função
+            return listaDeLivros;
+        } catch (error) {
+            console.log(`Erro ao acessar o modelo: ${error}`);
+            return null;
+        }
+    }
+
+
+
+    
+
+    
+
+   
+
+
 
 }
