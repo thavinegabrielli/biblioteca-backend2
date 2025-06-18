27a9844918d5c36434c67555084058952b7e04a8
Thavine Nardin <kimjojo542@gmail.com>
2025-06-18 16:20:54 -0300

    Documentação API para aluno, livro e emprestimo

--- src/routes.ts
+++ src/routes.ts
@@ -5,6 +5,9 @@ import LivroController from "./controller/LivroController";
 import EmprestimoController from "./controller/EmprestimoController";
 import UsuarioController from "./controller/UsuarioController";
 import { upload, uploadCapa } from "./config/multerConfig"; // caminho pode variar dependendo da estrutura
+import swaggerUi from "swagger-ui-express"; // Importa a API do swagger
+import swaggerOutput from "../docs/swagger_doc.json"; // Importa o arquivo de saída do swagger
+
 
 const router = express.Router();
 
@@ -32,5 +35,7 @@ router.put(SERVER_ROUTES.ATUALIZAR_EMPRESTIMO, EmprestimoController.atualizar);
 // Cadastro de Usuário com Upload de Imagem de Perfil
 router.post(SERVER_ROUTES.NOVO_USUARIO, upload.single('imagemPerfil'), UsuarioController.cadastrar);
 router.post (SERVER_ROUTES.NOVO_LIVRO, uploadCapa.single('imagemCapa'), LivroController.cadastrar);
+
+router.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerOutput)); // Rota para acessar a documentação
 
 export { router }
